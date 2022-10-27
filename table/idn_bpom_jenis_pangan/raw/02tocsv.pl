b5916247e8ccf98e4e7f868ec94f12674a4d8c57
Steven Haryanto (on netbook-dell-xps13) <stevenharyanto@gmail.com>
2022-10-27 16:26:07 +0700

    [bugfix] data.csv must not contain header

--- table/idn_bpom_jenis_pangan/raw/02tocsv.pl
+++ table/idn_bpom_jenis_pangan/raw/02tocsv.pl
@@ -1,2 +1,2 @@
 #!/bin/bash
-perl -pe'BEGIN { print "code,summary\n" } m!<option value="(\d+)">(.+) \(\d+\)! or die; $_ = "$1,\"$2\"\n";' 02*txt > ../data.csv
+perl -pe'm!<option value="(\d+)">(.+) \(\d+\)! or die; $_ = "$1,\"$2\"\n";' 02*txt > ../data.csv
